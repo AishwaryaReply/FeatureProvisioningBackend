9c3713e8b4b168b7e1ad5ad1b9dbfda4b719a217
ammurad <a.murad@reply.com>
2022-06-06 13:38:23 -0400

    factory.ts Files

--- src/lambdas/ServiceSchedulerManager/src/vehicle/vehicle-factory.ts
+++ src/lambdas/ServiceSchedulerManager/src/vehicle/vehicle-factory.ts
@@ -1,3 +1,25 @@
-export class VehicleFactory{
+import { CommonServices } from "gcv-common-services";
+import { GCVErrors } from "gcv-utils/lib/error/gcv-error";
+import { VehicleScheduler, VehicleHandler } from ".";
 
+export class VehicleFactory {
+    public static getVehicleHandler(): VehicleHandler {
+        return new VehicleHandler();
+    }
+
+    public static getVehcileScheduler(): VehicleScheduler {
+        return new VehicleScheduler();
+    }
+
+    public static getVehicleDiscoveryService(): CommonServices.VehicleDiscoveryService{
+        return new CommonServices.VehicleDiscoveryService();
+    }
+
+    public static getEnvironment() {
+        const ENVIRONMENT = process.env.stage;
+        if(!ENVIRONMENT){
+            throw new GCVErrors.SystemException('Failed to get environment stage!');
+        }
+        return ENVIRONMENT;
+    }
 }
